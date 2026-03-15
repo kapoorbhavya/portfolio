caf89ae78c22f77bd2ef3158c01be332c6da9196
Bhavya Kapoor <kapoorbhavya266@gmail.com>
2026-03-15 18:22:28 +0530

    Remove unused imports (useEffect, useRef)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
